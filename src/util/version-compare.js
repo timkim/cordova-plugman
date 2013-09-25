6b41fd3a39962a83c72bf732f1ffaa4feb0e03c6
Tim Kim <timk@adobe.com>
2013-09-25 15:13:27 -0700

    [CB-4872]-figuring out satisy function

--- src/util/version-compare.js
+++ src/util/version-compare.js
@@ -1,15 +1,16 @@
-function getVesionObject(versionString){
+function getVersionObject(versionString){
     var majorReg = /\d+/,
     minorReg = /\d+\.\d+/,
     patchReg = /\d+\.\d+\.\d+/;
     
-    var major=null,
-    minor=null,
-    patch= null;
+    var major = null,
+    minor = null,
+    patch = null;
     
     if(majorReg.test(versionString)){
         major = parseInt(versionString.match(majorReg)[0]);
     }
+    // make sure error out if major is null
     
     if(minorReg.test(versionString)){
         minor = parseInt(versionString.match(minorReg)[0].split('.')[1]);
@@ -55,22 +56,67 @@ function compare(numOne, numTwo, comparator){
             return (numOne >= numTwo);
         break;
         
-        case '=':
+        default:
             return (numOne == numTwo);
+    }
+}
+
+function checkVersionFormat(versionOut, versionRange){
+    var majorOk = false, 
+    minorOk = false, 
+    patchOk = false;
+    
+    if(versionOut.major!=null && versionRange.major!=null){
+        majorOk = true;
+    }else{
+        // error
+    }
+    
+    if(versionOut.minor!=null && versionRange.minor!=null){
+        minorOk = true;
+    }else{
+        // error
+    }
+    
+    if(versionOut.patch!=null && versionRange.patch!=null){
+        patchOk = true;
+    }else{
+        // error
+    }
+    
+    if(majorOk && minorOk && patchOk){
+        return 'toPatch';
+    }else if(majorOk && minorOk && !patchOk){
+        return 'toMinor';
+    }else if(majorOk && !minorOk && !patchOk){
+        return 'toMajor';
+    }
+}
+
+function satisfy(versionOut, versionRange, comparator){
+    var format = checkVersionFormat(versionOut, versionRange);
+    
+    switch(format){
+        case 'toMajor':
+            return compare(versionOut.major, versionRange.major, comparator);
+        break;
+        
+        case 'toMinor':
+            return true;
+        break;
+        
+        case 'toPatch':
+            return true;
         break;
     }
 }
-    
-function satisfy(versionOutput, versionSatisfy, comparator){
-       
-}
 
-module.exports = function(versionOutput, versionSatisfy){
-    var theVersionOutput = getVersionObject(versionOutput);
-    var theVersionSatisfy = getVersionObject(versionSatisfy);
-    var comparator = getComparator(versionSatisfy);
+module.exports = function(versionOut, versionRange){
+    var theVersionOut = getVersionObject(versionOut);
+    var theVersionRange = getVersionObject(versionRange);
+    var comparator = getComparator(versionRange);
     
-    return satisfy(theVersionOutput, theVersionSatisfy, comparator);
+    return satisfy(theVersionOut, theVersionRange, comparator);
 }
 
 
